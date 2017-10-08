9540e93f52b6b56984ce1b295779100cca2c76d3
Tim Anema <tim.anema@shopify.com>
2017-10-07 20:09:17 -0400

    Initial commit

---
+++ animation.go
@@ -0,0 +1,148 @@
+package spry
+
+// Animation defines a range of frames from the atlas to be played over a specified
+// amount of time.
+type Animation struct {
+	atlas         *Atlas
+	frames        []int
+	duration      float32
+	frameDuration float32
+	looping       bool
+	position      int
+	playing       bool
+	flippedH      bool
+	flippedV      bool
+	timer         float32
+}
+
+// FlipH will flip the animation horizontally while animating
+func (anim *Animation) FlipH() {
+	anim.flippedH = !anim.flippedH
+}
+
+// FlipV will flip the animation vertically while animating
+func (anim *Animation) FlipV() {
+	anim.flippedV = !anim.flippedV
+}
+
+// Pause will pause the animation on the current frame
+func (anim *Animation) Pause() {
+	anim.playing = false
+}
+
+// Resume will make the animation continue if it was paused
+func (anim *Animation) Resume() {
+	anim.playing = true
+}
+
+// GotoFrame will forward the animation to the specified position in the animation
+func (anim *Animation) GotoFrame(position int) {
+	anim.position = position
+	anim.timer = float32(position) * anim.frameDuration
+}
+
+// PauseAtEnd will forward the animation to the end and pause it
+func (anim *Animation) PauseAtEnd() {
+	anim.position = len(anim.frames) - 1
+	anim.timer = anim.duration
+	anim.Pause()
+}
+
+// PauseAtStart will return to the beginnign of the animation and pause it.
+func (anim *Animation) PauseAtStart() {
+	anim.position = 1
+	anim.timer = 0
+	anim.Pause()
+}
+
+// Update will take in the difference in time (dt) and update the current frame
+// it is displaying.
+func (anim *Animation) Update(dt float32) {
+	if !anim.playing {
+		return
+	}
+
+	anim.timer = anim.timer + dt
+	loops := int(anim.timer / anim.duration)
+	if loops != 0 {
+		anim.timer = anim.timer - anim.duration*float32(loops)
+	}
+
+	anim.position = int(float32(len(anim.frames)) * (anim.timer / anim.duration))
+}
+
+// Draw will draw the current frame of the animation to the screen. The arguments
+// satisfy the draw interface from amore
+func (anim *Animation) Draw(args ...float32) {
+	x, y, angle, sx, sy, ox, oy, kx, ky := anim.normalizeDrawCallArgs(args)
+	anim.atlas.Draw(anim.frames[anim.position], x, y, angle, sx, sy, ox, oy, kx, ky)
+}
+
+func (anim *Animation) normalizeDrawCallArgs(args []float32) (float32, float32, float32, float32, float32, float32, float32, float32, float32) {
+	var x, y, angle, sx, sy, ox, oy, kx, ky float32
+	sx = 1
+	sy = 1
+
+	argsLength := len(args)
+
+	switch argsLength {
+	case 9:
+		ky = args[8]
+		fallthrough
+	case 8:
+		kx = args[7]
+		if argsLength == 8 {
+			ky = kx
+		}
+		fallthrough
+	case 7:
+		oy = args[6]
+		fallthrough
+	case 6:
+		ox = args[5]
+		if argsLength == 6 {
+			oy = ox
+		}
+		fallthrough
+	case 5:
+		sy = args[4]
+		fallthrough
+	case 4:
+		sx = args[3]
+		if argsLength == 4 {
+			sy = sx
+		}
+		fallthrough
+	case 3:
+		angle = args[2]
+		fallthrough
+	case 2:
+		x = args[0]
+		y = args[1]
+	}
+
+	w, h := anim.GetDimensions()
+	if anim.flippedH {
+		sx = sx * -1
+		ox = w - ox
+		kx = kx * -1
+		ky = ky * -1
+	}
+
+	if anim.flippedV {
+		sy = sy * -1
+		oy = h - oy
+		kx = kx * -1
+		ky = ky * -1
+	}
+
+	return x, y, angle, sx, sy, ox, oy, kx, ky
+}
+
+// GetDimensions will return the width and height of the current frame
+func (anim *Animation) GetDimensions() (float32, float32) {
+	frameIndex := anim.frames[anim.position]
+	frame := anim.atlas.Frames[frameIndex]
+	_, _, w, h := frame.GetViewport()
+	return float32(w), float32(h)
+}
---
+++ atlas.go
@@ -0,0 +1,68 @@
+package spry
+
+import (
+	"github.com/tanema/amore/gfx"
+)
+
+// Atlas is the structure to serialize a spritesheet. You can retrieve and draw
+// single frames from the atlas
+type Atlas struct {
+	sheet  *gfx.Image
+	Frames []*gfx.Quad
+}
+
+// NewAtlas will build and return a new Atlas. frameWidth and frameHeight are to define
+// the size of each frame on the spritesheet. left and top define the offset from
+// the top left corner if there is padding around the outside of the spritesheet.
+// border defines the amount of pixels inbetween frames.
+// This method wil return an error if it has trouble loading the image at the
+// specified path.
+func NewAtlas(path string, frameWidth, frameHeight, left, top, border int32) (*Atlas, error) {
+	sheet, err := gfx.NewImage(path)
+	if err != nil {
+		return nil, err
+	}
+
+	sheetWidth, sheetHeight := sheet.Width, sheet.Height
+	framesHorizontal := int32(float32(sheetWidth) / float32(frameWidth))
+	framesVertical := int32(float32(sheetHeight) / float32(frameHeight))
+
+	atlas := &Atlas{
+		sheet:  sheet,
+		Frames: []*gfx.Quad{},
+	}
+
+	for y := int32(0); y < framesVertical; y++ {
+		for x := int32(0); x < framesHorizontal; x++ {
+			atlas.Frames = append(atlas.Frames, gfx.NewQuad(
+				left+x*frameWidth+x*border,
+				top+y*frameHeight+y*border,
+				frameWidth, frameHeight,
+				sheetWidth, sheetHeight,
+			))
+		}
+	}
+
+	return atlas, nil
+}
+
+// NewAnimation will create and return a new animation using this atlas as reference.
+// frames defines the frame indexes in the atlas. duration defines how long the animation
+// should play for. looping will define id the animation should loop when it gets
+// to the end.
+func (atlas *Atlas) NewAnimation(frames []int, duration float32, looping bool) *Animation {
+	return &Animation{
+		atlas:         atlas,
+		frames:        frames,
+		duration:      duration,
+		frameDuration: duration / float32(len(frames)),
+		looping:       looping,
+		playing:       true,
+	}
+}
+
+// Draw will draw the frame at the index supplied. the provided arguments fit the
+// amore draw interface.
+func (atlas *Atlas) Draw(index int, args ...float32) {
+	gfx.Drawq(atlas.sheet, atlas.Frames[index], args...)
+}
